af6a370673f519083454eb08889bd82e864be58b
naveenkkn03 <naveenkkn03@gmail.com>
2023-09-26 20:45:05 +0530

    Day-3-Task is Completed

--- compareJSON.js
+++ compareJSON.js
@@ -6,3 +6,5 @@ let ob2 = {
 };
 console.log(ob1 === ob2);
 console.log(JSON.stringify(ob1) === JSON.stringify(ob2));
+// console.log(Object.toJSON(ob1) == Object.toJSON(ob2));
+// console.log(_.isEqual(ob1, ob2));
